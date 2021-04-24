e9e922e33a6f9fc6fd96c682153574328373502f
Kevin Roan <kevinnroan@gmail.com>
2021-04-24 10:07:06 -0700

    for deploy

--- run.bat
+++
@@ -1,4 +0,0 @@
-cd src
-"C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito --new-window http://localhost:8000
-python -m http.server 8000
-@REM "C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito --new-window mytargetpage.com
